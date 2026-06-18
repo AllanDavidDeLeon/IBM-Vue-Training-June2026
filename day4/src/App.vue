8aa477b3092512b617fb77b2ffbc2efec4018d4e
Allan David De Leon <adc.deleon11@gmail.com>
2026-06-18 11:32:32 +0800

    Day 4 - Pinia integration

---
+++ day4/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TaskListView from './views/TaskListView.vue';
+</script>
+
+<template>
+  <TaskListView />
+</template>
